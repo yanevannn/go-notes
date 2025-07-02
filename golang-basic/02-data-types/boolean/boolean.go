a5c86085d507ac58265796c7b679d313fe4ac8f6
yanevannn <yanevan03@gmail.com>
2025-07-03 01:44:19 +0800

    remove all notes

--- golang-basic/02-data-types/boolean/boolean.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main (){
-	fmt.Println(true) //output: true
-	fmt.Println(false) //output: false
-	// boolean tidak menggunakan " " atau ' ' karena boolean adalah tipe data bukan string
-}
--- golang-basic/02-data-types/string/string.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "fmt"
-func main (){
-	fmt.Println("Halo ini adalah tipe data string menggunakan GOlang")
-	//output: Halo ini adalah tipe data string menggunakan GOlang
-}
